37315d626e964cf6de749d426c25d140cd2b077f
Daheer <suhayrid6@gmail.com>
2024-01-16 18:08:40 +0100

    ui change 3

--- app/components/navbar.js
+++ app/components/navbar.js
@@ -23,23 +23,22 @@ export function NavBar() {
     onOpen();
   };
   return (
-    <Navbar className='bg-transparent m-10' fluid rounded>
+    <Navbar className='bg-transparent lg:m-10 md:m-8 sm:m-5' fluid rounded>
       <NavbarBrand href="#">
         <img src="/logo.svg" className="mr-3 h-6 sm:h-9" alt="Keffi Pixel Press Logo" />
       </NavbarBrand>
-      <span className="self-center whitespace-nowrap lg:text-4xl md:text-2xl sm:text-md text-md text-white font-semibold dark:text-white">
+      <span className="self-center whitespace-nowrap lg:text-4xl md:text-2xl text-md text-white font-semibold dark:text-white">
         <span className={KollektifFontStyle.className}>
-          <span className="lg:text-6xl md:text-5xl sm:text-2xl text-xl">K</span>EFFI
-          <span className="lg:text-6xl md:text-5xl sm:text-2xl text-xl">P</span>IXEL
+          <span className="lg:text-6xl md:text-5xl text-xl">K</span>EFFI
+          <span className="lg:text-6xl md:text-5xl text-xl">P</span>IXEL
           &nbsp;
-          <span className="lg:text-6xl md:text-5xl sm:text-2xl text-xl">P</span>RESS
+          <span className="lg:text-6xl md:text-5xl text-xl">P</span>RESS
         </span>
       </span>
-      <div className="flex">
-
+      <div>
         <>
-          <div className="flex flex-wrap gap-3">
-            <Button onPress={() => handleOpen("blur")} variant='bordered' size='lg' className='text-white sm:text-md md:text-md md:p-2 p-2'>
+          <div className="flex flex-wrap">
+            <Button onPress={() => handleOpen("blur")} variant='bordered' size='small' className='text-white'>
               <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24" fill="none" stroke="#ffffff" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"><path d="M22 16.92v3a2 2 0 0 1-2.18 2 19.79 19.79 0 0 1-8.63-3.07 19.5 19.5 0 0 1-6-6 19.79 19.79 0 0 1-3.07-8.67A2 2 0 0 1 4.11 2h3a2 2 0 0 1 2 1.72 12.84 12.84 0 0 0 .7 2.81 2 2 0 0 1-.45 2.11L8.09 9.91a16 16 0 0 0 6 6l1.27-1.27a2 2 0 0 1 2.11-.45 12.84 12.84 0 0 0 2.81.7A2 2 0 0 1 22 16.92z"></path></svg>
               Contact us
             </Button >
