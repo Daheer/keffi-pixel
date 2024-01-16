2e5186923462bb30e10e94a8330263f62af4472f
Daheer <suhayrid6@gmail.com>
2024-01-16 19:47:11 +0100

    More UI Refinement

--- app/components/serviceCard.js
+++ app/components/serviceCard.js
@@ -3,7 +3,7 @@ import Link from "next/link";
 export function ServiceCard({ title, service, price, image }) {
   return (
     <Link href={`/${service}`} style={{ textDecoration: 'none' }}>
-      <div style={{ backgroundImage: `url(${image})` }} className="flex flex-col text-white text-md md:text-md lg:text-xl items-center justify-center lg:w-72 lg:h-72 md:w-60 md:h-60 sm:w-48 sm:h-48 w-48 h-48 rounded-lg shadow-lg">
+      <div style={{ backgroundImage: `url(${image})` }} className="flex flex-col text-center text-white text-lg md:text-lg lg:text-xl items-center justify-center lg:w-72 lg:h-72 md:w-60 md:h-60 sm:w-56 sm:h-56 w-56 h-56 rounded-lg shadow-lg">
         <h1 className="font-semibold">{title}</h1>
         {price !== "TBD" ? (
           <p className="text-center">Price: ₦{price}/unit</p>
